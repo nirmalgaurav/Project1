5d48891ab97bd5541add6edd8aa2234ee36c2faf
Gaurav Nirmal <103414603+nirmalgaurav@users.noreply.github.com>
2022-11-03 11:51:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+const form = document.querySelector('#searchForm');
+const res = document.querySelector('#tableResult');
+var upd;
+
+form.addEventListener('submit' , (e)=> {
+
+  e.preventDefault();
+  if(upd){
+    clearTimeout(upd);
+  }
+
+  const cType = form.elements.coinType.value;
+
+  console.log(cType);
+  fetchPrice(cType);
+
+})
+
+const fetchPrice = async(cType) =>{
+
+  const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${cType}?currency=INR`);
+  console.log(r);
+  const price =  r.data.coin.price;
+  const volume = r.data.coin.volume;
+  const change1hour = r.data.coin.priceChange1h;
+  const rank = r.data.coin.rank;
+  res.innerHTML = `<tr class="topRow">
+    <td>
+    <b>Properties</b>
+    </td>
+    <td><b>Values</b></td>
+  </tr>
+
+  <tr>
+    <td>
+      Rank
+    </td>
+    <td>${rank}</td>
+  </tr>
+
+  <tr>
+    <td>
+      Price
+    </td>
+    <td>${price}</td>
+  </tr>
+
+  <tr>
+    <td>
+      volume
+    </td>
+    <td>${volume}</td>
+  </tr>
+
+  <tr>
+    <td>
+      Price Change in 1 Hour
+    </td>
+    <td>${change1hour}</td>
+  </tr>
+`
+upd = setTimeout(()=>fetchPrice(cType),10000);
+
+
+}
